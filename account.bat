dc61fdfd1364f1782b757c7bcd4f85c7213a7433
JeallyRabbit <119430599+JeallyRabbit@users.noreply.github.com>
2022-12-06 13:02:49 +0100

    Delete account.bat

--- account.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Podaj haslo lokalnego admina
-powershell -command "$password = Read-Host -AsSecureString"
-powershell -command "net user Administrator $password"
-powershell -command "Disable-LocalUser $username"
-Pause
