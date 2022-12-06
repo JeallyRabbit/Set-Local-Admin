4f1a539e0c90071a9e47c375a8da5077fd394ffb
JeallyRabbit <119430599+JeallyRabbit@users.noreply.github.com>
2022-12-06 11:55:02 +0100

    Add files via upload

---
+++ Set_account_&_domain.ps1
@@ -0,0 +1,12 @@
+﻿echo "------------------------------------------Ustawianie konta lokalnego administratora-----------------------------------------"
+$password = Read-Host -AsSecureString "Podaj haslo lokalnego admina"
+net user Administrator $password
+Disable-LocalUser $username
+echo "-----------------Wylaczono obecnie zalogowane lokalne konto oraz uruchomiono konto lokalnego administratora-----------------"
+
+echo "------------------------------------------Dodawanie komputera do domeny C4CR.LOCAL------------------------------------------"
+$name = Read-Host "Podaj nowa nazwe komputera"
+Rename-Computer -NewName $name
+Add-computer -DomainName C4CR.LOCAL
+
+echo "--------------------------------------------------------KONIEC ZABAWY-------------------------------------------------------"
